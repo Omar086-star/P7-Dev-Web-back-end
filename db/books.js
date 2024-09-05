19e6f2d24bf7af1a581d15f91ad54d09f27e6f69
omar mulla <mullaomar086@gmail.com>
2024-09-05 09:11:01 +0200

    creer l'array de book et l'exporter.

---
+++ db/books.js
@@ -0,0 +1,16 @@
+const books = [{
+    userId: "1",
+    title: "The Lord of the Rings",
+    author: "J.R.R Tolkien",
+    imageUrl: "./the-lord.jpg",
+    year: 1954,
+    genre: "Fantasy",
+    rating: [{
+      userId: "1",
+      grade: 5
+    }],
+    averageRating: 5
+  }];
+  
+  module.exports = { books };
+  
