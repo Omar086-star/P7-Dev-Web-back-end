724047976f4b09e7bbeda6fef51d142bba6c0c8e
omar mulla <mullaomar086@gmail.com>
2024-09-05 09:10:05 +0200

    ajoute les functions de book avec diskStorage par "multer".

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-
+const { User , Book } = require('./db/mongo');
 const app = express();
+const bcrypt = require("bcrypt");
+const { books } = require('./db/books');
+const multer = require('multer');
+
+const storage=multer.diskStorage({
+  destination: function (req, file , cb){
+    cb(null , "uploads");
+  },
+  filename: function (req , file , cb){
+    const filename= file.originalname.toLocaleLowerCase() + Date.now() + ".jpg";
+    cb(null, Date.now() + "-" + filename)
+  }
+})
+
+const upload=multer({
+  storage:storage
+})
+
+
 const port = 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static("uploads"))
 
 let browserOpened = false;
 
 app.get('/', testLeProjet);
 app.post('/api/auth/signup', signUp);
 app.post('/api/auth/login', logIn);
+app.get('/api/books' , getBooks);
+app.post('/api/books',upload.single("image"), postBook);
+
+ 
 
 app.listen(port, async function () {
   console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
@@ -31,49 +55,85 @@ app.use((err, req, res) => {
   res.status(500).send('Quelque chose a mal tourné !');
 });
 
+// Les fonctions de signUP, logIN et getBooks
 
-// les function de signUP logIN
 function testLeProjet(req, res) {
   res.send('Hello World!');
 }
-const users = [];
-function signUp(req, res) {
-  const body = req.body;
+
+async function signUp(req, res) {
   const email = req.body.email;
   const password = req.body.password;
-  const userList = users.find((user)=>user.email===email);
-  if (userList!= null){
-    res.status(400).send("Email déjà existe ");
+
+  const userList = await User.findOne({ email: email });
+  if (userList != null) {
+    res.status(400).send("Email déjà existe");
     return;
   }
-  const user = {
-    email:email,
-    password:password
+
+  const user = { email: email, password: hashPassword(password) };
+
+  try {
+    await User.create(user);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Erreur serveur");
+    return;
   }
-  users.push(user)
-  console.log('Body:', body);
+
+  console.log('Body:', req.body);
   res.send("Sign UP");
 }
-const admin ={
-email:"mullaomar086@gmail.com",
-password:"MULLAomar086"
-}
-function logIn(req, res) {
+
+async function logIn(req, res) {
   console.log('Requête reçue sur /api/auth/login');
-  const body = req.body;
-  const userList=users.find((user)=>user.email===body.email);
-  if(userList==null){
-    res.status(401).send("mal écrire");
+  const userList = await User.findOne({ email: req.body.email });
+
+  if (userList == null) {
+    res.status(401).send("Email incorrect");
     return;
   }
-  const passwordList = userList.password;
-  if(passwordList!=body.password){
-    res.status(401).send("mal écrire");
+
+  if (!isPasswordCorrect(req.body.password, userList.password)) {
+    res.status(401).send("Mot de passe incorrect");
     return;
   }
- 
+
   res.send({
-    userId: "123",
+    userId: userList._id,
     token: "token"
   });
+}
+
+function hashPassword(password) {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+}
+
+function isPasswordCorrect(password, hash) {
+  return bcrypt.compareSync(password, hash);
+}
+
+function getBooks(req, res) {
+  res.send(books);
+};
+
+async function postBook(req,res){
+const file = req.file;
+console.log("file:" , file
+  
+)
+const body = req.body;
+const strigBook= req.body.book;
+const book = JSON.parse(strigBook);
+book.imageUrl = file.path;
+try{
+const result = await Book.create(book);
+console.log("resul :" ,result);
+res.send({message:'Book pupler' , book: result})
+}
+catch(e){
+  console.error(e);
+  res.status(500).send('Somthing went wrong' + e.message);
+}
 }
